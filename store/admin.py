bc04aeede73bdb67b0318d49977541d061536f9c
Ruhul Amin Parvez <ruhulaminparvez007@gmail.com>
2022-05-21 17:34:12 +0600

    Product Model Create and Admin Registered Product Models :grey_question:

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Product
 
-# Register your models here.
+# Register your models here
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['product_name', 'price', 'stock', 'category', 'created_date', 'modified_date', 'is_available']
+    prepopulated_fields = {'slug': ('product_name',)}
+    list_filter = ['created_date', 'modified_date']
+    search_fields = ['product_name']
+
+admin.site.register(Product, ProductAdmin)
