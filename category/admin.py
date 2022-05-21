9b57fc520a8f5fd0559601696558337098064868
Ruhul Amin Parvez <ruhulaminparvez007@gmail.com>
2022-05-21 17:21:35 +0600

    Add Category Images and Create New App Store :octocat:

--- category/admin.py
+++ category/admin.py
@@ -3,4 +3,9 @@ from .models import Category
 
 
 # Register your models here.
-admin.site.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('category_name',)}
+    list_display = ('category_name', 'slug')
+    
+
+admin.site.register(Category, CategoryAdmin)
--- choose_mart/settings.py
+++ choose_mart/settings.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
     #Installed Apps
     'category',
     'accounts',
+    'store',
 ]
 
 MIDDLEWARE = [
@@ -114,6 +115,9 @@ STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_DIRS = [ 
     'choose_mart/static',
  ]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
