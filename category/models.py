8bc88fef98027e10bc86e6b7acddb8851d7bb8dd
Ruhul Amin Parvez <ruhulaminparvez007@gmail.com>
2022-05-21 13:10:07 +0600

    Create Category App and it's Model :octocat:

---
+++ category/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+class Category(models.Model):
+    category_name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=100, unique=True)
+    description = models.TextField(max_length=255, blank=True)
+    cat_image = models.ImageField(upload_to='images/categories', blank=True)
+
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = 'Categories'
+
+
+    def __str__(self):
+        return self.category_name
