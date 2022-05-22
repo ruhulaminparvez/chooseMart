726afafc6501193bf716af1c8cf1b049661ac618
Ruhul Amin Parvez <ruhulaminparvez007@gmail.com>
2022-05-23 00:41:22 +0600

    Get URL for Products :grey_question:

--- store/models.py
+++ store/models.py
@@ -1,6 +1,7 @@
 from unicodedata import category
 from django.db import models
 from category.models import Category
+from django.urls import reverse
 
 
 # Create your models here.
@@ -14,7 +15,12 @@ class Product(models.Model):
     is_available = models.BooleanField(default=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     created_date = models.DateTimeField(auto_now_add=True)
-    modified_date = models.DateTimeField(auto_now=True)    
+    modified_date = models.DateTimeField(auto_now=True)
+
+
+    def get_url(self):
+        return reverse('product_detail', args=[self.category.slug, self.slug])
+
 
     def __str__(self):
         return self.product_name
