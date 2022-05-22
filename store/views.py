0dc44aefe8fc3d2f67bc49d1dcf45da18d707ef4
Ruhul Amin Parvez <ruhulaminparvez007@gmail.com>
2022-05-22 21:38:34 +0600

    Added Dynamic Store Items and Counter of total Items :octocat:

--- store/views.py
+++ store/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
 def store(request):
-    return render(request, 'store/store.html')
+    products = Product.objects.all().filter(is_available=True)
+    product_count = products.count()
+
+    context = {
+        'products': products,
+        'product_count': product_count
+    }
+    return render(request, 'store/store.html', context)
