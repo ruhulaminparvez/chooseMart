af34aef2230b8ccc359f60a0a8d2b5227a3f5833
Ruhul Amin Parvez <ruhulaminparvez007@gmail.com>
2022-05-27 19:20:35 +0600

    Fix Product Error Issues

--- cart/views.py
+++ cart/views.py
@@ -54,6 +54,8 @@ def remove_cart_item(request, product_id):
 
 def cart(request, total=0, quantity=0, cart_items=None):
     try:
+        tax = 0
+        grand_total = 0
         cart = Cart.objects.get(cart_id=_cart_id(request))
         cart_items = CartItem.objects.filter(cart=cart, active=True)
         for cart_item in cart_items:
--- store/views.py
+++ store/views.py
@@ -20,7 +20,7 @@ def store(request, category_slug=None):
         paged_products = paginator.get_page(page)
         product_count = products.count()
     else:
-        products = Product.objects.all().filter(is_available=True)
+        products = Product.objects.all().filter(is_available=True).order_by('id')
         paginator = Paginator(products, 6)
         page = request.GET.get('page')
         paged_products = paginator.get_page(page)
