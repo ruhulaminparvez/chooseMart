4a278847d6f181becc4b4c8554e281fe2aa01f8e
Ruhul Amin Parvez <ruhulaminparvez007@gmail.com>
2022-05-27 18:00:16 +0600

    Counter context processor to count products :octocat:

--- choose_mart/settings.py
+++ choose_mart/settings.py
@@ -57,6 +57,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'category.context_processors.menu_links',
+                'cart.context_processors.counter',
             ],
         },
     },
