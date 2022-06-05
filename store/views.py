3d7c04009c6ca4f95cc12d8e237870e6c4ab14fc
Ruhul Amin Parvez <ruhulaminparvez007@gmail.com>
2022-06-05 15:21:02 +0600

    Search and Footer Added :octocat:

--- store/views.py
+++ store/views.py
@@ -1,6 +1,9 @@
+from venv import create
+from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render
 from .models import Product
 from category.models import Category
+from django.db.models import Q
 
 from cart.models import CartItem
 from cart.views import _cart_id
@@ -46,3 +49,17 @@ def product_detail(request, category_slug, product_slug):
     }
 
     return render(request, 'store/product_detail.html', context)
+
+
+
+def search(request):
+    if 'keyword' in request.GET:
+        keyword = request.GET['keyword']
+        if keyword:
+            products = Product.objects.order_by('-created_date').filter(Q(description__icontains=keyword) | Q(product_name__icontains=keyword))
+    
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'store/store.html', context)
