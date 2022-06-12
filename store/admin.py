870d101ff0c7f5130764ba7c622f52c24f5d9a63
Ruhul Amin Parvez <ruhulaminparvez007@gmail.com>
2022-06-12 15:42:57 +0600

    Product Variation Model :octocat:

--- store/admin.py
+++ store/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, Variation
 
 # Register your models here
 class ProductAdmin(admin.ModelAdmin):
@@ -8,4 +8,14 @@ class ProductAdmin(admin.ModelAdmin):
     list_filter = ['created_date', 'modified_date']
     search_fields = ['product_name']
 
+
+class VariationAdmin(admin.ModelAdmin):
+    list_display = ['product', 'variation_category', 'variation_value', 'is_active']
+    list_editable = ['is_active']
+    list_filter = ['product', 'variation_category', 'variation_value', 'is_active']
+    search_fields = ['product', 'variation_category', 'variation_value']
+
+
+
 admin.site.register(Product, ProductAdmin)
+admin.site.register(Variation, VariationAdmin)
--- store/models.py
+++ store/models.py
@@ -24,3 +24,21 @@ class Product(models.Model):
 
     def __str__(self):
         return self.product_name
+
+
+variation_category_choice = (
+    ('size', 'Size'),
+    ('color', 'Color'),
+)
+
+
+class Variation(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    variation_category = models.CharField(max_length=100, choices=variation_category_choice)
+    variation_value = models.CharField(max_length=100)
+    is_active = models.BooleanField(default=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+
+
+    def __unicode__(self):
+        return self.product
