a199cf993c810db2ed901e2c37235ae7ae8d4cd1
Ruhul Amin Parvez <ruhulaminparvez007@gmail.com>
2022-06-12 16:13:00 +0600

    Dynamic  Variation Values Worked :octocat:

--- cart/views.py
+++ cart/views.py
@@ -12,10 +12,10 @@ def _cart_id(request):
     return cart
 
 def add_cart(request, product_id):
-    color = request.GET['color']
-    size = request.GET['size']
-    return HttpResponse(color + ' ' + size)
-    exit()
+    if request.method == 'POST':
+        color = request.POST['color']
+        size = request.POST['size']
+        print( color, size )
 
     product = Product.objects.get(id=product_id) #get product
     try:
--- store/models.py
+++ store/models.py
@@ -26,6 +26,14 @@ class Product(models.Model):
         return self.product_name
 
 
+class VariationManager(models.Manager):
+    def colors(self):
+        return super(VariationManager, self).filter(variation_category='color', is_active=True)
+
+    def sizes(self):
+        return super(VariationManager, self).filter(variation_category='size', is_active=True)
+
+
 variation_category_choice = (
     ('size', 'Size'),
     ('color', 'Color'),
@@ -39,6 +47,7 @@ class Variation(models.Model):
     is_active = models.BooleanField(default=True)
     created_date = models.DateTimeField(auto_now_add=True)
 
+    objects =  VariationManager()
 
     def __unicode__(self):
         return self.product
